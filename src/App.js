0c3ae40b9f193fb583eb626602a1b1fa55933901
Arthuria Ex <arduinomk2016@gmail.com>
2021-09-12 18:18:49 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
+import SearchTerm from './components/SearchTerm/SearchTerm';
+import Card from './components/Card/Card';
+import Home from './views/Home';
+import NotFound from './views/NotFound';
+import Contact from './views/Contact';
+import Products from './views/Products';
+import ProductDetail from './views/ProductDetail';
+import Message from './components/Message/Message';
+import CheckOut from './views/CheckOut';
+import Register from './components/Register/Register';
+import SignIn from './components/SignIn/SignIn';
+import UpdateInfo from './components/UpdateInfo/UpdateInfo'
+import Info from './views/Info';
+import ScrollButton from './components/ScrollButton/ScrollButton';
+import AddProduct from './components/AddProduct/AddProduct';
+import { useSelector } from 'react-redux';
+
 
 function App() {
+  const {isOpenRegister, isOpenSignIn} = useSelector(state => state.account);
+  const {user, isOpenUpdateInfo} = useSelector(state => state.account);
+  const {isOpenFormProduct} = useSelector(state => state.admin);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav></Nav>
+        <SearchTerm></SearchTerm>
+        <Card></Card>
+        <Message></Message>
+        <ScrollButton></ScrollButton>
+        {isOpenRegister && <Register></Register>}
+        {isOpenSignIn && <SignIn></SignIn>}
+        {isOpenUpdateInfo && <UpdateInfo></UpdateInfo>}
+        {isOpenFormProduct && <AddProduct/>}
+        <Switch>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+          <Route path="/checkout">
+            <CheckOut/>
+          </Route>
+          <Route path="/product/:id">
+            <ProductDetail/>
+          </Route>
+          <Route path="/products">
+            <Products/>
+          </Route>
+          <Route path="/contact">
+            <Contact/>
+          </Route>
+          {user.email !== '' &&
+            <Route path="/info">
+              <Info/>
+            </Route>
+          }
+          <Route path="*">
+            <NotFound/>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
---
+++ src/views/Info.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserInfo from '../components/UserInfo/UserInfo'
+import UserReceipts from '../components/UserReceipts/UserReceipts'
+
+const Info = () => {
+    return (
+        <>
+            <UserInfo></UserInfo>   
+            <UserReceipts></UserReceipts>
+        </>
+    )
+}
+
+export default Info
